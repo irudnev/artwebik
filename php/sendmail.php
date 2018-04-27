da62e2b4693e833cc789d8350627d033dff9bdfb
ihor <rudnev.dn@mail.ru>
2018-04-27 17:15:21 +0300

    files

--- php/sendmail.php
+++ php/sendmail.php
@@ -1,10 +1,14 @@
 ﻿<?php
+//что менять
+$chars_bad = array(";", ".", "?", "/", ":");
+//на что менять
+$chars_good = array("[tchszpt]", "[tch]", "[qst]", "[del]", "[dvtch]");
 //Проверка правильности ввода EMAIL
 if(trim($_POST['email']) == '')  {
 $email = "-";
 $hasError = true;
 } else {
-$email = htmlspecialchars(trim($_POST['email']));
+$email = str_replace($chars_bad, $chars_good, trim($_POST['email']));
 $hasError = false;
 }
 //Проверка наличия телефона
@@ -12,12 +16,12 @@ if(trim($_POST['phone']) == '') {
 $phone = "-";
 $hasError = $hasError == true;
 } else {
-$phone = htmlspecialchars(trim($_POST['Phone']));
+$phone = str_replace($chars_bad, $chars_good, trim($_POST['phone']));
 $hasError = false;
 }
 //Если ошибок нет, отправить email
 if($hasError == false) {
-$emailTo = 'rudnev.dn@mail.ru';
+$emailTo = 'artwebik@gmail.com';
 $body = "New website order.\n\nClient: $email \nPhone: $phone";
 $name = "$email";
 $headers = "Content-type: text/plain; charset='utf-8'r";
