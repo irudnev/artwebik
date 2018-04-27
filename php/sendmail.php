3f10c4b9289237b67af63af1f0221adb9e6f78aa
ihor <rudnev.dn@mail.ru>
2018-04-27 16:35:24 +0300

    message

--- php/sendmail.php
+++ php/sendmail.php
@@ -1,33 +1,24 @@
 ﻿<?php
-//  //Проверка Поля ИМЯ
-//  if(trim($_POST['Name']) == '') {
-//  $hasError = true;
-//  } else {
-//  $name = trim($_POST['Name']);
-//  }
 //Проверка правильности ввода EMAIL
 if(trim($_POST['email']) == '')  {
-$hasError = true;
+$email = "-";
 } else {
-$email = trim($_POST['email']);
+$email = htmlspecialchars(trim($_POST['email']));
 }
-// //Проверка наличия телефона
-//  if(trim($_POST['Phone']) == '') {
-//  $hasError = true;
-//  } else {
-//  $phone = trim($_POST['Phone']);
-//  }
-//Если ошибок нет, отправить email
-if(!isset($hasError)) {
+//Проверка наличия телефона
+if(trim($_POST['phone']) == '') {
+$phone = "-";
+} else {
+$phone = htmlspecialchars(trim($_POST['Phone']));
+}
+
 $emailTo = 'rudnev.dn@mail.ru';
-$body = "Новый заказ сайта: $email";
-$name = "name: $email";
-// $body = "Name: $name \n\nEmail: $email \n\nPhone: $phone";
+$body = "New website order.\n\nClient: $email \nPhone: $phone";
+$name = "$email";
 $headers = "Content-type: text/plain; charset='utf-8'r";
-$headers .= 'From: IDIS <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $email ."r";
+$headers .= 'From: <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $email ."r";
 mail($emailTo, $name, $body, $headers);
-$emailSent = true;
-}
+
 $redirect = isset($_SERVER['HTTP_REFERER'])? $_SERVER['HTTP_REFERER']:'../index.html';
 header("Location: $redirect");
 exit();
